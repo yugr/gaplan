a57da4a8a5fec590df14d554cee55275301154d0
Yury Gribov <tetra2005@gmail.com>
2020-04-04 14:12:11 +0300

    Update copyrights.

--- gaplan/__main__.py
+++ gaplan/__main__.py
@@ -2,7 +2,7 @@
 
 # The MIT License (MIT)
 # 
-# Copyright (c) 2016-2018 Yury Gribov
+# Copyright (c) 2016-2020 Yury Gribov
 # 
 # Use of this source code is governed by The MIT License (MIT)
 # that can be found in the LICENSE.txt file.
