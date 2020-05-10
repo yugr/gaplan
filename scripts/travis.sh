ee42ed1726f29cc976ceeadede2dfd0255cc6184
Yury Gribov <tetra2005@gmail.com>
2020-05-10 20:55:06 +0300

    Enable codecov support.

---
+++ scripts/travis.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Copyright 2020 Yury Gribov
+#
+# The MIT License (MIT)
+# 
+# Use of this source code is governed by MIT license that can be
+# found in the LICENSE.txt file.
+
+set -eu
+
+if test -n "${TRAVIS:-}"; then
+  set -x
+fi
+
+cd $(dirname $0)/..
+
+python3 -mpip install .
+
+export PYTEST="${PYTEST:-python3 -mpytest}"
+$PYTEST gaplan
